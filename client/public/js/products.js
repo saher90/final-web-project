5ab04e7083617fe0d50bb9f8a0c58aad04db1df0
saher90 <sahernaoum@gmail.com>
2020-08-02 19:22:45 +0300

    update in product.html/css/js

---
+++ client/public/js/products.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+
+    $('.btn-submit').click(function () {
+        var elmId = $(this).closest(".text-wrap").attr("id");
+        alert(elmId);
+        console.log(elmId)
+        return false;
+    })
+    $('.class-quantity').on('change', function () {
+        var amount = $(this).val()
+        if (amount > 0) {
+            amount *= 20
+            $(this).closest(".form-wrap").find('#price').html(`${amount}$`)
+        } else
+            $(this).val(1)
+        return false;
+    });
+
+})
