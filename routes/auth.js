a0a67be453b5f37df8db843fffbe03f465690c0e
saher90 <sahernaoum@gmail.com>
2020-08-12 02:41:15 +0300

    updated to open in phone screen

--- routes/auth.js
+++ routes/auth.js
@@ -208,7 +208,6 @@ MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
         res.redirect('/');
     });
     router.get('/products-items', function(req, res) {
-        console.log("products")
         const collection = db.collection('products')
         collection.find().toArray(function(err, docs) {
             assert.equal(err, null);
@@ -321,7 +320,7 @@ MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
 
     })
     router.get('/is-admin', checkuser, (req, res) => {
-        console.log("is-admin")
+
         return res.send(req.admin);
         /* if (!req.body) {
              return res.send({ admin: "empty" });
@@ -472,7 +471,7 @@ MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
             req.admin = "empty"
             return next();
         }
-        console.log("checkuser")
+
         if (!req.user) {
             req.admin = "empty"
             return next();
